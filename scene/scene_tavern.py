a079076828bcb934ae470f199b8f82fa37569333
seedjyh <seedjyh@gmail.com>
2018-11-17 17:32:23 +0800

    新增：增加“酒馆”场景的判断。

---
+++ scene/scene_tavern.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: seedjyh@gmail.com
+# Create date: 2018/11/17
+import time
+
+from pywindow import Position
+from pywindow.colour import Colour
+from pywindow.window import get_window_handle
+from scene import Scene, MatchRule
+
+
+class SceneTavern(Scene):
+    def __init__(self):
+        pass
+
+    def match(self, window):
+        rules = [
+            MatchRule(position=Position(153, 41), colour=Colour("4E69A7")),
+        ]
+        for rule in rules:
+            if not window.get_pixel_color(rule.position).similar_to(rule.colour):
+                return False
+        return True
+
+
+if __name__ == "__main__":
+    window_text = "Cadria Item Shop"
+    window_handle = get_window_handle(window_text)
+    window_handle.set_foreground()
+    time.sleep(1)
+    scene = SceneTavern()
+    if scene.match(window_handle):
+        print("match!")
+    else:
+        print("NOT match!")
